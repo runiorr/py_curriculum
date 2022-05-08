032a250ea49f6b68a202cde3ec4868756c052de3
runiorr <joseluisrjunior@gmail.com>
2022-05-08 19:20:17 -0300

    correct encoding

--- template/resumeBuilder.py
+++ template/resumeBuilder.py
@@ -1,4 +1,3 @@
-from components.component import Component
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
@@ -10,7 +9,6 @@ class ResumeBuilder:
         # Set font
         plt.rcParams['font.family'] = 'sans-serif'
         plt.rcParams['font.sans-serif'] = 'STIXGeneral'
-        plt.rcParams['text.usetex'] = True
         fig, ax = plt.subplots(figsize=(8.5, 11))
         # Decorative Lines
         ax.axvline(x=.5, ymin=0, ymax=1, color='#007ACC', alpha=0.0, linewidth=50)
