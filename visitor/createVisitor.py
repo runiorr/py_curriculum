12546b9c8e5e9453e1c9d70dfefbc2099ec72368
runiorr <joseluisrjunior@gmail.com>
2022-05-08 03:08:26 -0300

    removing print

--- visitor/createVisitor.py
+++ visitor/createVisitor.py
@@ -29,7 +29,6 @@ class CreateVisitor:
             for item in range(items[0]):
                 project.setTitle()
                 project.setDesc()
-                print(project.desc)
         else: self.visitProjects(project) 
 
     def visitWorks(self, work: Works):
@@ -39,7 +38,6 @@ class CreateVisitor:
                 work.setTitle()
                 work.setTime()
                 work.setDesc()
-                print(work.desc)
         else: self.visitWorks(work)
 
     def visitEducations(self, education: Educations):
