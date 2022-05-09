94d2291438e4afd6f377fc2c0c7b22009270433c
runiorr <joseluisrjunior@gmail.com>
2022-05-09 14:55:53 -0300

    testing new method to make pdf

--- testing/test.py
+++ testing/test.py
@@ -105,7 +105,29 @@ html_string = html_string.format(
 
 # print(html_string)
 
-with open("template/resume.html", "w") as file:
+with open("testing/test.html", "w") as file:
     file.write(html_string)
+
+from xhtml2pdf import pisa             
+
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# Define your data
+source_html = open('testing/test.html')
+output_filename = "testing/test.pdf"
+convert_html_to_pdf(source_html, output_filename)
 
 print("DONE")
